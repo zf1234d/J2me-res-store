30c3c41ff08261f09acd7ac0256a64f4293ccb0b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-02 00:10:20 +0800

    Initial commit

---
+++ app/src/main/java/com/mBZo/jar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mBZo.jar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
